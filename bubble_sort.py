39eb05a055c86c005d47bb11ea5dd1551f02a1ca
Sergey Grishin <sergey.grishin@revelsystems.com>
2020-10-15 13:30:05 +0300

    bubble_sort is added

---
+++ bubble_sort.py
@@ -0,0 +1,12 @@
+arr = [1,5,90,56,0,34,2,3,7]
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        for j in range(n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+
+print(bubble_sort(arr))
