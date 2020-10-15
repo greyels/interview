b02f8df04897fff889a3ab6b9a7a11d3b0d4af16
Sergey Grishin <sergey.grishin@revelsystems.com>
2020-10-15 13:00:20 +0300

    initial commit

---
+++ quick_sort.py
@@ -0,0 +1,19 @@
+arr = [1,5,90,56,0,34,2,3,7]
+
+def quick_sort(arr):
+    less = []
+    equal = []
+    greater = []
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    for i in arr:
+        if i < pivot:
+            less.append(i)
+        elif i == pivot:
+            equal.append(i)
+        elif i > pivot:
+            greater.append(i)
+    return quick_sort(less) + equal + quick_sort(greater)
+
+print(quick_sort(arr))
