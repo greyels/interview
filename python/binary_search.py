343f10a23e06542de61c4f4020b3940832213e04
greyels <greyels@gmail.com>
2021-05-03 00:32:54 +0300

    binary_search.py fix

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,13 +1,13 @@
 # recursive solution
 def binary_search_rec(arr, left, right, item):
     if right >= left:
-        mid = (right + left) // 2;
+        mid = (right + left) // 2
         if arr[mid] == item:
             return mid
         elif arr[mid] > item:
-            return binary_search_rec(arr, left, mid-1, item)
+            return binary_search_rec(arr, left, mid - 1, item)
         elif arr[mid] < item:
-            return binary_search_rec(arr, mid+1, right, item)
+            return binary_search_rec(arr, mid + 1, right, item)
     else:
         return
 
@@ -17,9 +17,9 @@ def binary_search_iter(arr, item):
     left = 0
     right = len(arr) - 1
     while right >= left:
-        mid = (right + left) // 2;
+        mid = (right + left) // 2
         if arr[mid] == item:
-            return mid 
+            return mid
         elif arr[mid] < item:
             left = mid + 1
         elif arr[mid] > item:
@@ -27,8 +27,8 @@ def binary_search_iter(arr, item):
     return
 
 
-array = [1,3,5,6,9,99,1987]
-item = 5
+array = [1, 3, 5, 6, 9, 99, 1987]
+my_item = 5
 
-print(binary_search_iter(array, item))
-print(binary_search_rec(array, 0, len(array)-1, item))
+print(binary_search_iter(array, my_item))
+print(binary_search_rec(array, 0, len(array) - 1, my_item))
