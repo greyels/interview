686b14c5259c4350e14e11a04a603feac5a6c000
Sergey Grishin <s.grishin@accenture.com>
2023-08-29 23:29:58 +0200

    More tasks from leetcode and common algos

--- dsa/dp/maximum_subarray.py
+++ dsa/dp/maximum_subarray.py
@@ -1,12 +1,19 @@
 class Solution:
     # Dynamic programming
     def maxSubArrayDP(self, nums):
-
+        cur_sum, max_sum = 0, -float('inf')
+        for num in nums:
+            cur_sum += num
+            max_sum = max(cur_sum, max_sum)
+            if cur_sum < 0:
+                cur_sum = 0
+        return max_sum
 
     # Divide and conquer
     def maxSubArrayDC(self, nums):
-        pass
+
 
 nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
 assert Solution().maxSubArrayDP(nums), 6
+assert Solution().maxSubArrayDC(nums), 6
 print("OK")
--- dsa/sorting/bubble_sort.py
+++ dsa/sorting/bubble_sort.py
@@ -1,9 +1,16 @@
 def bubble_sort(arr):
-    for i in range(len(arr) - 1):
-        for j in range(len(arr) - 1 - i):
+    n = len(arr)
+    swapped = False
+    for i in range(n - 1):
+        for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            return arr
     return arr
 
+
 array = [1, 5, 90, 56, 0, 34, 2, 3, 7]
-print(bubble_sort(array))
+assert bubble_sort(array), [0, 1, 2, 3, 5, 7, 34, 56, 90]
+print("OK")
