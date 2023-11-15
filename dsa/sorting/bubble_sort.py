ec2566d5b7a0589e6e3108eca47cc7a339f47e04
Sergey Grishin <s.grishin@accenture.com>
2023-11-15 22:54:14 +0100

    Updated sorting algos

--- dsa/sorting/bubble_sort.py
+++ dsa/sorting/bubble_sort.py
@@ -1,16 +1,16 @@
 def bubble_sort(arr):
     n = len(arr)
-    swapped = False
     for i in range(n - 1):
+        swapped = False
         for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
                 swapped = True
         if not swapped:
-            return arr
-    return arr
+            break
 
 
-array = [1, 5, 90, 56, 0, 34, 2, 3, 7]
-assert bubble_sort(array), [0, 1, 2, 3, 5, 7, 34, 56, 90]
+arr = [1, 5, 90, 56, 0, 34, 2, 3, 7]
+bubble_sort(arr)
+assert arr == [0, 1, 2, 3, 5, 7, 34, 56, 90]
 print("OK")
