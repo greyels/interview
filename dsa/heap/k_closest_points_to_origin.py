32260171b3d9d936f09a9cd8ab19b3c2072b0001
Sergey Grishin <s.grishin@accenture.com>
2023-11-18 01:44:59 +0100

    Heap issues

---
+++ dsa/heap/k_closest_points_to_origin.py
@@ -0,0 +1,20 @@
+import heapq
+from typing import List
+
+
+class Solution:
+    # MaxHeap usage -> O(nlog(k)), because heapq.pop and heapq.push give O(log(k))
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        heap = []
+        for x, y in points:
+            # Beacuse we need a MaxHeap (multiply on -1)
+            dist = -(x * x + y * y)
+            if len(heap) == k:
+                heapq.heappushpop(heap, (dist, x, y))
+            else:
+                heapq.heappush(heap, (dist, x, y))
+        return [[x, y] for (dist, x, y) in heap]
+
+    # Sorting -> O(nlog(n))
+    def kClosestSorted(self, points: List[List[int]], k: int) -> List[List[int]]:
+        return sorted(points, key=lambda x: (x[0] ** 2 + x[1] ** 2))[:k]
