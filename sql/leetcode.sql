fc07d21fdd68d0aff2672130a8800b3e8cfe61b6
Sergey Grishin <s.grishin@accenture.com>
2025-03-21 19:08:39 +0100

    more SQL

--- sql/leetcode.sql
+++ sql/leetcode.sql
@@ -175,3 +175,19 @@ from (
         lead(num) over (order by id) as next_num
     from Logs) as cons_nums
 where num = prev_num and num = next_num
+
+-- https://leetcode.com/problems/product-price-at-a-given-date
+select product_id, 10 as price
+from Products
+group by product_id
+having min(change_date) > "2019-08-16"
+union all
+select product_id, new_price as price
+from Products
+where (product_id, change_date) in
+(
+    select product_id, max(change_date)
+    from Products
+    where change_date <= "2019-08-16"
+    group by product_id
+)
